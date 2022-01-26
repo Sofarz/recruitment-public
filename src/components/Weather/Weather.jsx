3a8b4762b2dd6e5240d8b8db04196dc9779b5312
Sofarz <sofia.wallen@gmail.com>
2022-01-26 15:38:56 +0100

    fix indentation

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -16,11 +16,11 @@ const Weather = () => {
         
         fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=a17480f70f0d4368ad0b5eabd0e37b66`)
         .then(response => response.json())
-        .then(json => {
-            if (json.cod !== 200) {
+        .then(response => {
+            if (response.cod !== 200) {
                 throw new Error ()
             } else {
-                setCurrentWeather(json);
+                setCurrentWeather(response);
                 setErrorMessage(false);
             }     
         })
@@ -56,19 +56,19 @@ const Weather = () => {
             <div>
                 {errorMessage && <ErrorMessage />}
                 {currentWeather.cod === 200 && !errorMessage ? 
-                <div>
-                <h1 className="location">{currentWeather.name}</h1>
-                    <p className="date">{new Date().toDateString()}</p>
-                    <div className="temperature">{Math.round(currentWeather.main.temp - 273.25) + '°C'}</div>
-                    <div className="icon-description-container">
-                        <img 
-                            src={`http://openweathermap.org/img/w/${currentWeather.weather[0].icon}.png`} 
-                            alt='weather icon' 
-                        />
-                        <h4 className="weather-description">
-                            {currentWeather.weather[0].description}
-                        </h4>  
-                    </div>
+                    <div>
+                        <h1 className="location">{currentWeather.name}</h1>
+                        <p className="date">{new Date().toDateString()}</p>
+                        <div className="temperature">{Math.round(currentWeather.main.temp - 273.25) + '°C'}</div>
+                        <div className="icon-description-container">
+                            <img 
+                                src={`http://openweathermap.org/img/w/${currentWeather.weather[0].icon}.png`} 
+                                alt='weather icon' 
+                            />
+                            <h4 className="weather-description">
+                                {currentWeather.weather[0].description}
+                            </h4>  
+                        </div>
                     </div>
                 : null 
                 }
